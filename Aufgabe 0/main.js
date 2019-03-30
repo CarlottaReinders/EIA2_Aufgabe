44e13f9b210bb2f6663d7fbe2ed5b3a055e45d2a
Carlotta Reinders <carlotta-reinders@web.de>
2019-03-30 22:38:11 +0100

    Update

--- Aufgabe 0/main.js
+++ Aufgabe 0/main.js
@@ -1,7 +1,5 @@
-function Clickme() {
-    var person = prompt("Please enter your name");
-    if (person != null) {
-        document.getElementById("Hello").innerHTML =
-            "Hello " + person + "! How are you today?";
-    }
+var person = prompt("Please enter your name");
+if (person != null) {
+    document.getElementById("Hello").innerHTML =
+        "Hello " + person;
 }
--- Aufgabe 0/main.ts
+++ Aufgabe 0/main.ts
@@ -1,9 +1,6 @@
-
-function Clickme {
-    var person = prompt ("Please enter your name");
+var person = prompt ("Please enter your name");
 
 if (person !=null) {
     document.getElementById("Hello").innerHTML =
-    "Hello " + person + "! How are you today?";
-}
+    "Hello " + person;
 }
