478733e899dff4c84585d4dc452367bbf65b1c0f
Carlotta Reinders <carlotta-reinders@web.de>
2019-04-11 11:49:38 +0200

    Updates

--- Aufgabe 2/js/main.js
+++ Aufgabe 2/js/main.js
@@ -1,161 +1,129 @@
 /* Array für das Kartendeck*/
 var h7 = {
-    kartendeck: true,
     bild: 1,
     value: 7,
 };
 var h8 = {
-    kartendeck: true,
     bild: 1,
     value: 8,
 };
 var h9 = {
-    kartendeck: true,
     bild: 1,
     value: 9,
 };
 var h10 = {
-    kartendeck: true,
     bild: 1,
     value: 10,
 };
 var hB = {
-    kartendeck: true,
     bild: 1,
     value: 11,
 };
 var hD = {
-    kartendeck: true,
     bild: 1,
     value: 12,
 };
 var hK = {
-    kartendeck: true,
     bild: 1,
     value: 13,
 };
 var hA = {
-    kartendeck: true,
     bild: 1,
     value: 14,
 };
 var kr7 = {
-    kartendeck: true,
     bild: 2,
     value: 7,
 };
 var kr8 = {
-    kartendeck: true,
     bild: 2,
     value: 8,
 };
 var kr9 = {
-    kartendeck: true,
     bild: 2,
     value: 9,
 };
 var kr10 = {
-    kartendeck: true,
     bild: 2,
     value: 10,
 };
 var krB = {
-    kartendeck: true,
     bild: 2,
     value: 11,
 };
 var krD = {
-    kartendeck: true,
     bild: 2,
     value: 12,
 };
 var krK = {
-    kartendeck: true,
     bild: 2,
     value: 13,
 };
 var krA = {
-    kartendeck: true,
     bild: 2,
     value: 14,
 };
 var p7 = {
-    kartendeck: true,
     bild: 3,
     value: 7,
 };
 var p8 = {
-    kartendeck: true,
     bild: 3,
     value: 8,
 };
 var p9 = {
-    kartendeck: true,
     bild: 3,
     value: 9,
 };
 var p10 = {
-    kartendeck: true,
     bild: 3,
     value: 10,
 };
 var pB = {
-    kartendeck: true,
     bild: 3,
     value: 11,
 };
 var pD = {
-    kartendeck: true,
     bild: 3,
     value: 12,
 };
 var pK = {
-    kartendeck: true,
     bild: 3,
     value: 13,
 };
 var pA = {
-    kartendeck: true,
     bild: 3,
     value: 14,
 };
 var ka7 = {
-    kartendeck: true,
     bild: 4,
     value: 7,
 };
 var ka8 = {
-    kartendeck: true,
     bild: 4,
     value: 8,
 };
 var ka9 = {
-    kartendeck: true,
     bild: 4,
     value: 9,
 };
 var ka10 = {
-    kartendeck: true,
     bild: 4,
     value: 10,
 };
 var kaB = {
-    kartendeck: true,
     bild: 4,
     value: 11,
 };
 var kaD = {
-    kartendeck: true,
     bild: 4,
     value: 12,
 };
 var kaK = {
-    kartendeck: true,
     bild: 4,
     value: 13,
 };
 var kaA = {
-    kartendeck: true,
     bild: 4,
     value: 14,
 };
@@ -167,8 +135,8 @@ var obersteKarte;
 function ausgegebeneKarten() {
     var kartenanzahl = 0;
     do {
-        kartenanzahl = parseInt(prompt("Kartenanzahl angeben (Zahl zwischen 5 und 7 eingeben)"));
-    } while (isNaN(kartenanzahl) || kartenanzahl > 7 || kartenanzahl < 5);
+        kartenanzahl = parseInt(prompt("Kartenanzahl angeben (Zahl von 1 bis 6 eingeben)"));
+    } while (isNaN(kartenanzahl) || kartenanzahl > 6 || kartenanzahl < 1);
     console.log("kartenanzahl: " + kartenanzahl);
     for (var n = 0; n < kartenanzahl; n++) { /* solange die Variable n kleiner ist als die Anzahl der Karten, wird die Funktion "karteZiehen" ausgeführt */
         karteZiehen();
@@ -180,35 +148,29 @@ function ausgegebeneKarten() {
 }
 /* Funktion zum Karte ziehen */
 function karteZiehen() {
-    var i = Math.floor(Math.random() * 31);
-    while (deck[i].kartendeck == false) {
-        i = Math.floor(Math.random() * 31); /* eine zufällige Karte von den Stellen 0 bis 31 im Array wird ausgegeben */
-    }
-    hand[hand.length] = deck[i];
-    deck[i].kartendeck = false;
+    var i = Math.floor(Math.random() * (deck.length - 1));
+    hand.push(deck[i]); /* Karte aus dem Deck wird den Handkarten hinzugefügt */
+    deck.splice(i, 1);
 }
 /* Funktion für den Kartenstapel */
 function kartenstapelGenerieren() {
-    var i = Math.floor(Math.random() * 31);
-    while (deck[i].kartendeck == false) {
-        i = Math.floor(Math.random() * 31);
-    }
+    var i = Math.floor(Math.random() * (deck.length - 1));
     obersteKarte = deck[i];
-    deck[i].kartendeck = false;
+    deck.splice(i, 1);
     var write = "";
-    write += "<div>";
+    write += "<div class=\"cards\">";
     switch (obersteKarte.bild) {
         case 1:
-            write += "<img src=\"img/herz.jpg\" class=\"symbol\" alt=\"\u2665\">\n                <div class=\"rot\">";
+            write += "<img src=\"img/herz.jpeg\" class=\"symbol\" alt=\"\u2665\">\n                <div class=\"rot\">";
             break;
         case 2:
-            write += "<img src=\"img/kreuz.jpg\" class=\"symbol\" alt=\"\u2663\" \n                <div class=\"schwarz\">";
+            write += "<img src=\"img/kreuz.jpeg\" class=\"symbol\" alt=\"\u2663\" \n                <div class=\"schwarz\">";
             break;
         case 3:
-            write += "<img src=\"img/pik.jpg\" class=\"symbol\" alt=\"\u2660\"\n                <div class=\"schwarz\">";
+            write += "<img src=\"img/pik.jpeg\" class=\"symbol\" alt=\"\u2660\"\n                <div class=\"schwarz\">";
             break;
         case 4:
-            write += "<img src=\"img/karo.jpg\" class=\"symbol\" alt=\"\u2666\" \n                <div class=\"rot\">";
+            write += "<img src=\"img/karo.jpeg\" class=\"symbol\" alt=\"\u2666\" \n                <div class=\"rot\">";
             break;
         default:
             console.log("Bild konnte nicht geladen werden");
@@ -254,16 +216,16 @@ function handkarten() {
         var write = "<div class=\"cards\">";
         switch (hand[n].bild) {
             case 1:
-                write += "<img src=\"img/herz.jpg\" class=\"symbol\" alt=\"\u2665\">\n                <div class=\"rot\">";
+                write += "<img src=\"img/herz.jpeg\" class=\"symbol\" alt=\"\u2665\">\n                <div class=\"rot\">";
                 break;
             case 2:
-                write += "<img src=\"img/kreuz.jpg\" class=\"symbol\" alt=\"\u2663\" \n                <div class=\"schwarz\">";
+                write += "<img src=\"img/kreuz.jpeg\" class=\"symbol\" alt=\"\u2663\" \n                <div class=\"schwarz\">";
                 break;
             case 3:
-                write += "<img src=\"img/pik.jpg\" class=\"symbol\" alt=\"\u2660\"\n                <div class=\"schwarz\">";
+                write += "<img src=\"img/pik.jpeg\" class=\"symbol\" alt=\"\u2660\"\n                <div class=\"schwarz\">";
                 break;
             case 4:
-                write += "<img src=\"img/karo.jpg\" class=\"symbol\" alt=\"\u2666\" \n                <div class=\"rot\">";
+                write += "<img src=\"img/karo.jpeg\" class=\"symbol\" alt=\"\u2666\" \n                <div class=\"rot\">";
                 break;
             default:
                 console.log("Bild konnte nicht geladen werden");
--- Aufgabe 2/js/main.ts
+++ Aufgabe 2/js/main.ts
@@ -1,6 +1,5 @@
 /* Die Karten initialisieren */
 interface karten {
-    kartendeck: boolean;
     bild: number;
     value: number;
 }
@@ -8,193 +7,161 @@ interface karten {
 /* Array für das Kartendeck*/ 
 
 let h7:karten={
-    kartendeck: true,
     bild:1,
     value:7,
 }
 
 let h8:karten={
-    kartendeck: true,
     bild: 1,
     value: 8,
 }
 
 let h9:karten={
-    kartendeck: true,
     bild: 1,
     value: 9,
 }
 
 let h10:karten={
-    kartendeck: true,
     bild: 1,
     value: 10,
 }
 
 let hB:karten={
-    kartendeck: true,
     bild: 1,
     value: 11,
 }
 
 let hD:karten={
-    kartendeck: true,
     bild: 1,
     value: 12,
 }
 
 let hK:karten={
-    kartendeck: true,
     bild: 1,
     value: 13,
 }
 
 let hA:karten={
-    kartendeck: true,
     bild: 1,
     value: 14,
 }
 
 let kr7:karten={
-    kartendeck: true,
     bild: 2,
     value: 7,
 }
 
 let kr8:karten={
-    kartendeck: true,
     bild: 2,
     value: 8,
 }
 
 let kr9:karten={
-    kartendeck: true,
     bild: 2,
     value: 9,
 }
 
 let kr10:karten={
-    kartendeck: true,
     bild: 2,
     value: 10,
 }
 
 let krB:karten={
-    kartendeck: true,
     bild: 2,
     value: 11,
 }
 
 let krD:karten={
-    kartendeck: true,
     bild: 2,
     value: 12,
 }
 
 let krK:karten={
-    kartendeck: true,
     bild: 2,
     value: 13,
 }
 
 let krA:karten={
-    kartendeck: true,
     bild: 2,
     value: 14,
 }
 
 let p7:karten={
-    kartendeck: true,
     bild: 3,
     value: 7,
 }
 
 let p8:karten={
-    kartendeck: true,
     bild: 3,
     value: 8,
 }
 
 let p9:karten={
-    kartendeck: true,
     bild: 3,
     value: 9,
 }
 
 let p10:karten={
-    kartendeck: true,
     bild: 3,
     value: 10,
 }
 
 let pB:karten={
-    kartendeck: true,
     bild: 3,
     value: 11,
 }
 
 let pD:karten={
-    kartendeck: true,
     bild: 3,
     value: 12,
 }
 
 let pK:karten={
-    kartendeck: true,
     bild: 3,
     value: 13,
 }
 
 let pA:karten={
-    kartendeck: true,
     bild: 3,
     value: 14,
 }
 
 let ka7:karten={
-    kartendeck: true,
     bild: 4,
     value: 7,
 }
 
 let ka8:karten={
-    kartendeck: true,
     bild: 4,
     value: 8,
 }
 
 let ka9:karten={
-    kartendeck: true,
     bild: 4,
     value: 9,
 }
 
 let ka10:karten={
-    kartendeck: true,
     bild: 4,
     value: 10,
 }
 
 let kaB:karten={
-    kartendeck: true,
     bild: 4,
     value: 11,
 }
 
 let kaD:karten={
-    kartendeck: true,
     bild: 4,
     value: 12,
 }
 
 let kaK:karten={
-    kartendeck: true,
     bild: 4,
     value: 13,
 }
 
 let kaA:karten={
-    kartendeck: true,
     bild: 4,
     value: 14,
 }
@@ -209,14 +176,14 @@ let obersteKarte:karten;
 
 
 /* Funktion für die Anzahl der Karten, die man bekommt*/
-function ausgegebeneKarten() {
+function ausgegebeneKarten():void {
     let kartenanzahl: number = 0;
 
     do { 
-        kartenanzahl = parseInt(prompt("Kartenanzahl angeben (Zahl zwischen 5 und 7 eingeben)")); 
+        kartenanzahl = parseInt(prompt("Kartenanzahl angeben (Zahl von 1 bis 6 eingeben)")); 
         }
     
-    while(isNaN(kartenanzahl) || kartenanzahl > 7 || kartenanzahl < 5);
+    while(isNaN(kartenanzahl) || kartenanzahl > 6 || kartenanzahl < 1);
 
     console.log("kartenanzahl: " +kartenanzahl);
 
@@ -232,47 +199,39 @@ function ausgegebeneKarten() {
 }
 
 /* Funktion zum Karte ziehen */
-function karteZiehen() {
-    let i:number = Math.floor(Math.random() * 31);
-
-    while (deck[i].kartendeck==false)
-        {
-            i = Math.floor(Math.random() * 31);  /* eine zufällige Karte von den Stellen 0 bis 31 im Array wird ausgegeben */
-        }
-    hand[hand.length]=deck[i]; 
-    deck[i].kartendeck = false;
+function karteZiehen():void {
+    let i:number = Math.floor(Math.random() * (deck.length-1));
+    hand.push(deck[i]); /* Karte aus dem Deck wird den Handkarten hinzugefügt */
+    deck.splice(i,1);
 }
 
 
 /* Funktion für den Kartenstapel */
-function kartenstapelGenerieren() {
-    let i:number = Math.floor(Math.random() * 31);
-
-    while (deck[i].kartendeck==false) {
-        i = Math.floor(Math.random() * 31);
-    }
-
+function kartenstapelGenerieren():void {
+    let i:number = Math.floor(Math.random() * (deck.length-1));
     obersteKarte=deck[i];
-    deck[i].kartendeck = false;
+    deck.splice(i,1);
+
 
     let write:string = "";
-        write += `<div>`;
+        write += `<div class="cards">`;
+
 
         switch (obersteKarte.bild) {
             case 1:
-                write += `<img src="img/herz.jpg" class="symbol" alt="♥">
+                write += `<img src="img/herz.jpeg" class="symbol" alt="♥">
                 <div class="rot">`;
                 break;
             case 2:
-                write += `<img src="img/kreuz.jpg" class="symbol" alt="♣" 
+                write += `<img src="img/kreuz.jpeg" class="symbol" alt="♣" 
                 <div class="schwarz">`;
                 break;
             case 3:
-                write += `<img src="img/pik.jpg" class="symbol" alt="♠"
+                write += `<img src="img/pik.jpeg" class="symbol" alt="♠"
                 <div class="schwarz">`;
                 break;
             case 4:
-                write += `<img src="img/karo.jpg" class="symbol" alt="♦" 
+                write += `<img src="img/karo.jpeg" class="symbol" alt="♦" 
                 <div class="rot">`;
                 break;
             default:
@@ -314,7 +273,7 @@ document.getElementById("Stapel").innerHTML = `${write}`;
 
 
 
-function deckGenerieren(){
+function deckGenerieren():void{
     document.getElementById("Deck").innerHTML = `<div class="cards">
     <img src="img/rückseite.jpeg" alt="MISSING TEXTURE" class="kartenrückseite">
     </div>`;     
@@ -322,7 +281,7 @@ function deckGenerieren(){
 
 
 /* Funktion für die Handkarten */
-function handkarten() {
+function handkarten():void {
     document.getElementById("kartenAufHand").innerHTML = "";
 
     for (var n:number = 0; n<hand.length; n++){
@@ -330,19 +289,19 @@ function handkarten() {
 
         switch (hand[n].bild){
             case 1:
-                write += `<img src="img/herz.jpg" class="symbol" alt="♥">
+                write += `<img src="img/herz.jpeg" class="symbol" alt="♥">
                 <div class="rot">`;
                 break;
             case 2:
-                write += `<img src="img/kreuz.jpg" class="symbol" alt="♣" 
+                write += `<img src="img/kreuz.jpeg" class="symbol" alt="♣" 
                 <div class="schwarz">`;
                 break;
             case 3:
-                write += `<img src="img/pik.jpg" class="symbol" alt="♠"
+                write += `<img src="img/pik.jpeg" class="symbol" alt="♠"
                 <div class="schwarz">`;
                 break;
             case 4:
-                write += `<img src="img/karo.jpg" class="symbol" alt="♦" 
+                write += `<img src="img/karo.jpeg" class="symbol" alt="♦" 
                 <div class="rot">`;
                 break;
             default:
