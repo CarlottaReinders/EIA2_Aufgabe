c5803ef25f4cff049002cf6a3ecca2c3d04d3abc
Carlotta Reinders <carlotta-reinders@web.de>
2019-05-12 16:28:37 +0200

    bug fixes

--- Aufgabe7/server/Server.js
+++ Aufgabe7/server/Server.js
@@ -1,25 +1,29 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Http = require("http"); // Importiere alle Inhalte aus "http" als Typ Http.
-var L05_Server;
-(function (L05_Server) {
-    console.log("Starting server"); // "Starting server" wird auf der Konsole ausgegeben.
-    var port = Number(process.env.PORT); // Die Variable "port" vom typ number wird erstellt
-    if (!port) // Wenn Port nicht der Fall ist, wird die nächste Zeile ausgeführt
-        port = 8100; // Port wird zu 8100
-    var server = Http.createServer(); // Die Variable "server" vom Typ Http.Server wird erstellt und dieser Variable wird die Funktion createServer() zugewiesen
-    server.addListener("request", handleRequest); // "server" wird ein listener hinzugefügt. Der Listener hört auf einen request und führt handleRequest aus.
-    server.addListener("listening", handleListen); // "server" wird ein listener hinzugefügt. Der Listener hört auf listening und führt handleListen aus.
-    server.listen(port); // Ein Listener wird erstellt. Er hört auf die oben definierte Variable "port".
-    function handleListen() {
-        console.log("Listening"); // "Listening" wird an die Console ausgegeben
-    } // Die Funktion handleListen wird geschlossen
-    function handleRequest(_request, _response) {
-        console.log("I hear voices!"); // In der Konsole wird "I hear voices!" ausgegeben
-        _response.setHeader("content-type", "text/html; charset=utf-8"); // Header-Werte für ein Header-Objekt wird festgelegt. Preexistierender Header-Wert wird ersetzt 
-        _response.setHeader("Access-Control-Allow-Origin", "*"); // Header-Wert für Access Control wird festgelet. "*" lässt jeden Origin zu (anstelle von <origin> für spezifische Origins oder "null" für keinen möglichen Origin)
-        _response.write(_request.url); // Vom Server wurde eine URL empfangen, die ins _response geschrieben wird 
-        _response.end(); // Der Server erhält eine Bestätigung, dass der request vollständig ist.
-    } // Funktion handleRequest wird geschlossen
-})(L05_Server || (L05_Server = {})); // Namespace "L05 Server" wird geschlossen
-//# sourceMappingURL=server.js.map
+const Http = require("http"); // http-Objekt wird aus "http" importiert; TS wird vermittelt, dass Node verwendet wird
+const Url = require("url");
+console.log("Starting server"); // in der Konsole wird "Starting Server" ausgegeben
+let port = Number(process.env.PORT); // eine neue Variable vom Typ Number und mit dem Namen "port" wird erstellt, auf die der Server hören soll
+if (!port) // wenn port nicht richtig/anders ist, soll nächste Zeile ausgelesen werden
+    port = 8100; //port wird der Wert 8100 zugewiesen
+let server = Http.createServer(); // eine neue Variable vom Typ Http.Server und mit dem Namen "server" wird erstellt
+server.addListener("request", handleRequest); // der Variable "server" wird ein Listener zugefügt, der auf "request" hört und dann die Funktion "handleRequest" ausführt
+server.addListener("listening", handleListen); // der Variable "server" wird ein Listener zugefügt, der auf "listening" hört und dann die Funktion "handleListen" ausführt
+server.listen(port); // die Variable "server" hört jetzt auf die Variable "port"
+function handleListen() {
+    console.log("Listening"); // in der Konsole wird "Listening" ausgegeben, sobald die Funktion "handleListen" ausgeführt wird
+} // die Funktion "handleListen" wird geschlossen
+function handleRequest(_request, _response) {
+    console.log("I hear voices!"); // in der Konsole wird "I hear voices!" ausgegeben, sobald die Funktion "handleRequest" ausgeführt wird
+    console.log(_request.url);
+    _response.setHeader("content-type", "text/html; charset=utf-8"); // Header-Werte für Header-Objekt werden definiert; die Contentart, die Art des Dokuments "text/html" und die Unicode-Codierung "charset=utf-8" sind vorgegeben
+    _response.setHeader("Access-Control-Allow-Origin", "*"); // "Acess-Control-Allow-Origin" wird als Header-Wert festgelegt, erlaubt Webbrowsern oder Clients den Zugriff über einen anderen Server oder andere Domain
+    // _response.write(_request.url); // _request.url wird in _response.write in der URL geschrieben, vom Server so empfangen --- 2.4 Was hinter Schrägstrich in der URL eingetragen wird, steht dann auch im Browser 
+    //console.log (_request.url);
+    _response.write("<h3>Wir bestätigen hiermit folgende Artikel:</h3>");
+    let url = Url.parse(_request.url, true);
+    for (let key in url.query)
+        _response.write("<p>" + key + url.query[key] + "</p> <br>");
+    _response.end(); // _response wird beendet; der Server bekommt die Nachricht, dass request nun fertig ist
+} // die Funktion "handleRequest" wird geschlossen
+//# sourceMappingURL=Server.js.map
--- Aufgabe7/server/Server.ts
+++ Aufgabe7/server/Server.ts
@@ -1,41 +1,37 @@
-import * as Http from "http"; 								// Importiere alle Inhalte aus "http" als Typ Http.
+import * as Http from "http"; // http-Objekt wird aus "http" importiert; TS wird vermittelt, dass Node verwendet wird
 import * as Url from "url";
 
-namespace L05_Server {										// Erstelle Namespace "L05 Server"
-	console.log("Starting server");							// "Starting server" wird auf der Konsole ausgegeben.
-	let port: number = Number(process.env.PORT);			// Die Variable "port" vom typ number wird erstellt
-	if (!port)												// Wenn Port nicht der Fall ist, wird die nächste Zeile ausgeführt
-		port = 8100;										// Port wird zu 8100
 
-	let server: Http.Server = Http.createServer();			// Die Variable "server" vom Typ Http.Server wird erstellt und dieser Variable wird die Funktion createServer() zugewiesen
-	server.addListener("request", handleRequest);			// "server" wird ein listener hinzugefügt. Der Listener hört auf einen request und führt handleRequest aus.
-	server.addListener("listening", handleListen);			// "server" wird ein listener hinzugefügt. Der Listener hört auf listening und führt handleListen aus.
-	server.listen(port);									// Ein Listener wird erstellt. Er hört auf die oben definierte Variable "port".
+console.log("Starting server"); // in der Konsole wird "Starting Server" ausgegeben
+let port: number = Number(process.env.PORT); // eine neue Variable vom Typ Number und mit dem Namen "port" wird erstellt, auf die der Server hören soll
+if (!port) // wenn port nicht richtig/anders ist, soll nächste Zeile ausgelesen werden
+	port = 8100; //port wird der Wert 8100 zugewiesen
 
-	function handleListen(): void {							// Die Funktion handleListen wird definiert
-		console.log("Listening");							// "Listening" wird an die Console ausgegeben
-	}														// Die Funktion handleListen wird geschlossen
+let server: Http.Server = Http.createServer(); // eine neue Variable vom Typ Http.Server und mit dem Namen "server" wird erstellt
+server.addListener("request", handleRequest);  // der Variable "server" wird ein Listener zugefügt, der auf "request" hört und dann die Funktion "handleRequest" ausführt
+server.addListener("listening", handleListen); // der Variable "server" wird ein Listener zugefügt, der auf "listening" hört und dann die Funktion "handleListen" ausführt
+server.listen(port); // die Variable "server" hört jetzt auf die Variable "port"
 
-	function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void { 	// Funktion handleRequest wird definiert. Übergabeparameter: _request und _response. Rückgabewert: None. 
-		console.log("I hear voices!"); 						// In der Konsole wird "I hear voices!" ausgegeben
+function handleListen(): void { // eine neue Funktion vom Typ void und mit dem Namen "handleListen" wird erstellt
+	console.log("Listening"); // in der Konsole wird "Listening" ausgegeben, sobald die Funktion "handleListen" ausgeführt wird
+} // die Funktion "handleListen" wird geschlossen
 
-		_response.setHeader("content-type", "text/html; charset=utf-8");    // Header-Werte für ein Header-Objekt wird festgelegt. Preexistierender Header-Wert wird ersetzt 
-		_response.setHeader("Access-Control-Allow-Origin", "*");			// Header-Wert für Access Control wird festgelet. "*" lässt jeden Origin zu (anstelle von <origin> für spezifische Origins oder "null" für keinen möglichen Origin)
+function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void { // eine neue Funktion vom Typ void und mit dem Namen "handleRequest" wird erstellt, request_ speichert eingehende Nachrichten von port, _response speichert die Antwort vom Server
+	console.log("I hear voices!"); // in der Konsole wird "I hear voices!" ausgegeben, sobald die Funktion "handleRequest" ausgeführt wird
+	console.log(_request.url);
+	_response.setHeader("content-type", "text/html; charset=utf-8"); // Header-Werte für Header-Objekt werden definiert; die Contentart, die Art des Dokuments "text/html" und die Unicode-Codierung "charset=utf-8" sind vorgegeben
+	_response.setHeader("Access-Control-Allow-Origin", "*"); // "Acess-Control-Allow-Origin" wird als Header-Wert festgelegt, erlaubt Webbrowsern oder Clients den Zugriff über einen anderen Server oder andere Domain
 
-		//_response.write(_request.url);
-		//console.log(_request.url);							// Vom Server wurde eine URL empfangen, die ins _response geschrieben wird 
 
-		_response.write("Deine Bestellung wurde entgegen genommen!");
-		let url:Url.UrlWithParsedQuery = Url.parse(_request.url, true);
+	// _response.write(_request.url); // _request.url wird in _response.write in der URL geschrieben, vom Server so empfangen --- 2.4 Was hinter Schrägstrich in der URL eingetragen wird, steht dann auch im Browser 
+	//console.log (_request.url);
 
-		for (let key in url.query) { 
-			_response.write("p" + key + ":" + url.query[key] + "</p>");
-			console.log(key);
-			console.log(url.query[key]);
-		}
-		
-		console.log(_request.url);
+	_response.write("<h3>Wir bestätigen hiermit folgende Artikel:</h3>");
+	let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
+	for (let key in url.query)
+		_response.write("<p>" + key + url.query[key] + "</p> <br>");
 
-		_response.end();									// Der Server erhält eine Bestätigung, dass der request vollständig ist.
-	}														// Funktion handleRequest wird geschlossen
-}															// Namespace "L05 Server" wird geschlossen
+
+
+	_response.end(); // _response wird beendet; der Server bekommt die Nachricht, dass request nun fertig ist
+}
